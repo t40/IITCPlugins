c58fab2b1e8a1d635644df002d65d330c0024648
t40 <info@t40.ch>
2020-12-16 11:39:06 +0100

    Update polygon-portals-list.meta.js

--- polygon-portals-list.meta.js
+++ polygon-portals-list.meta.js
@@ -7,9 +7,9 @@
 // @updateURL      https://raw.githubusercontent.com/chrislennon/IITCPlugins/master/polygon-portals-list.meta.js
 // @downloadURL    https://raw.githubusercontent.com/chrislennon/IITCPlugins/master/polygon-portals-list.user.js
 // @description    [jonatkins-2014-05-15-0004] Display a sortable list of all visible portals within a polygon. drawTools Required.
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
