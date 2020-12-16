b5d4497d6d145a4c5fba5ea6bbbc9f5f69bb4b4b
t40 <info@t40.ch>
2020-12-16 11:33:00 +0100

    Update poly-fly-links.user.js

--- poly-fly-links.user.js
+++ poly-fly-links.user.js
@@ -6,10 +6,10 @@
 // @updateURL      https://raw.githubusercontent.com/Seraphli/IITCPlugins/master/poly-fly-links.meta.js
 // @downloadURL    https://raw.githubusercontent.com/Seraphli/IITCPlugins/master/poly-fly-links.user.js
 // @description    [Seraphli-2017-10-17-1641] Calculate how to link the portals to create the largest tidy set of nested fields. Enable from the layer chooser. Limits to a polygon. drawTools Required.
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
 
